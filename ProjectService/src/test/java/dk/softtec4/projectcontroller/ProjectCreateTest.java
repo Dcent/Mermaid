51d3ef23891b8eb8623b830ea69352f3a76734f5
Dcent <renemsorensen@gmail.com>
2017-10-06 22:23:25 +0200

    Features/frontpage (#6)
    * created first test will test build server now
    
    * created website project
    
    * merge
    
    * merge
    
    * clean up commit
    
    * clean up commit
    
    * clean up commit
    
    * clean up
    
    * created gradle project
    
    * new project
    
    * created new test
    
    * created new class and cleaned the project

---
+++ ProjectService/src/test/java/dk/softtec4/projectcontroller/ProjectCreateTest.java
@@ -0,0 +1,14 @@
+package dk.softtec4.projectcontroller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ProjectCreateTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

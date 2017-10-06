cfb37667fe07fa1228fa3e642d1181686aaceb57
Dcent <renemsorensen@gmail.com>
2017-10-06 16:36:48 +0200

    created first test will test build server now (#1)

---
+++ UserService/src/test/dk/softtec4/UserModifiyTest.java
@@ -0,0 +1,14 @@
+package test.dk.softtec4;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UserModifiyTest {
+
+	@Test
+	public void test() {
+		assertEquals("", 0, 0);
+	}
+
+}
